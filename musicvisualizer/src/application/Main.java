b1e644b902f858dafa1b862bab89c3dceee5d808
tobi <tobias.rohrer@outlook.com>
2017-01-05 15:59:07 +0100

    javax.sound ist die Java Sound API, denke die können wir verwenden

--- musicvisualizer/src/application/Main.java
+++ musicvisualizer/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javax.sound.*;
 
 
 public class Main extends Application {
