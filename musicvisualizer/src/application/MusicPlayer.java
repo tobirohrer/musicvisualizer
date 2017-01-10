b478aafd625a3f4f6cfcd4cee58cc18bef6c8891
tobi <tobias.rohrer@outlook.com>
2017-01-10 18:41:26 +0100

    Loop der solange wie das AudioFile läuft gebaut.

--- musicvisualizer/src/application/MusicPlayer.java
+++ musicvisualizer/src/application/MusicPlayer.java
@@ -1,5 +1,6 @@
 package application;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -8,39 +9,48 @@ import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 
 public class MusicPlayer {
-	public void playClip(String audioFile)
-	{
-	    try
-	    {
-	        AudioInputStream audioInputStream =
-	            AudioSystem.getAudioInputStream(
-	                this.getClass().getResourceAsStream(audioFile));
-	        DataLine.Info info = 
-	            new DataLine.Info( Clip.class, audioInputStream.getFormat() );
-	        Clip clip = (Clip) AudioSystem.getLine(info);
-	        clip.addLineListener(new LineListener() {
-	                public void update(LineEvent e) {
-	                    if (e.getType() == LineEvent.Type.STOP) {
-	                        synchronized(clip) {
-	                            clip.notify();
-	                        }
-	                    }
-	                }
-	            });
-	        clip.open(audioInputStream);        
-	        
-	        clip.setFramePosition(0);
-
-	        clip.start();
-	        synchronized (clip) {
-	            clip.wait();
-	        }
-	        clip.drain();
-	        clip.close();
-	    }
-	    catch (Exception ex)
-	    {
-	        ex.printStackTrace();
-	    }
+	public void playClip(String audioFile) {
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(this.getClass().getResourceAsStream(audioFile));
+			
+			AudioFormat originalFormat = audioInputStream.getFormat();
+			
+			System.out.println("Das Format des audioFiles ist " + originalFormat);
+			
+			DataLine.Info info = new DataLine.Info(Clip.class, audioInputStream.getFormat());
+			
+			Clip clip = (Clip) AudioSystem.getLine(info);
+			
+			clip.addLineListener(new LineListener() {
+				public void update(LineEvent e) {
+					if (e.getType() == LineEvent.Type.STOP) {
+				;
+						synchronized (clip) {
+							clip.notify();
+						}
+					}
+				}
+			});
+			clip.open(audioInputStream);
+			
+			clip.setFramePosition(0);
+			clip.start();
+			
+			long frameLength = clip.getFrameLength();
+			frameLength = frameLength -1000; 
+			
+			while(clip.getFramePosition() < frameLength){
+				//doSomething
+			}
+			
+			synchronized (clip) {
+				clip.wait();
+			}
+			clip.drain();
+			clip.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
